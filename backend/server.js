a8a8d62a48591a2c96b627fa22130ba119a887fa
Sasikiran-31 <kirankumar.2001@yahoo.in>
2025-03-23 15:54:15 -0400

    Testing the post call

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,20 @@
 import express from "express";
 import dotenv from 'dotenv'
 import { connectDB } from "./config/db.js";
-import Product from "./models/prodcut.model.js";
+import Product from "./models/product.model.js";
 dotenv.config();
 
 const app = express();
 
-
+app.use(express.json());
 
 app.get('/',(req, res) => {
     res.send('Backend is working');
 });
 
-app.post("/products", async(req, res) => {
+app.post("/api/products", async(req, res) => {
   const product = await Product.create(req.body);
-  if (!product.name || !product.image || !product.price) {
+  if (!product.name || !product.price || !product.image) {
     return res.status(400).json({ success :false,message: "Please fill all fields" });
   }
 
