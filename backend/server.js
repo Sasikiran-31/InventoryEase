097dd49fda071633a714f731a688eaebac460bbe
Sasikiran-31 <kirankumar.2001@yahoo.in>
2025-03-24 12:01:48 -0400

    .env changes

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,20 @@ import express from "express";
 import dotenv from 'dotenv'
 import { connectDB } from "./config/db.js";
 import productRoutes from "./Routes/model.routes.js";
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
+
 app.use("/api/products", productRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     connectDB();
-  console.log('Server is running on http://localhost:3000');
+  console.log("Server is running on http://localhost:5000",+ PORT);
 });
 
 
