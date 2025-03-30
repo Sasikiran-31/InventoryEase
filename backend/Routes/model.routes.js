ea6492a03320bc3c42ad63b716a74ea05644c48b
Sasikiran-31 <kirankumar.2001@yahoo.in>
2025-03-29 23:13:51 -0400

    front-end setup

--- backend/Routes/model.routes.js
+++ backend/Routes/model.routes.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import { createProduct, getProducts, deleteProduct, updateProduct} from '../Controllers/product.controller.js';
+import { createProduct, getProducts, deleteProduct, updateProduct } from '../Controllers/product.controller.js';
 
 
 const router = express.Router();
 
 
-router.get('/',getProducts);
-router.post('/',createProduct);
-router.delete('/:id',deleteProduct);
+router.get('/', getProducts);
+router.post('/', createProduct);
+router.delete('/:id', deleteProduct);
 router.put('/:id', updateProduct);
 
 export default router;
