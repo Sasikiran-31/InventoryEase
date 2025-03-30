a0e982a4a403ffd0514bbbc58035a8cd79b8b223
Sasikiran-31 <kirankumar.2001@yahoo.in>
2025-03-30 17:32:57 -0400

    Update createPage.jsx

--- frontend/front-end/src/pages/createPage.jsx
+++ frontend/front-end/src/pages/createPage.jsx
@@ -1,72 +1,79 @@
-import { Container, Stack, Typography, TextField, Box, Button } from "@mui/material";
+import { Container, Stack, Typography, TextField, Box, Button, Snackbar, Alert } from "@mui/material";
 import { useState } from "react";
 
 import { useProduct } from "../store/product.js";
 
-const createPage = () => {
-    const [product, setProduct] = useState({
-        name: "",
-        price: "",
-        image: ""
-    });
+const CreatePage = () => {
+  const [product, setProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
 
-    const { createProduct } = useProduct();
-    const handleProduct = async () => {
-        const { success , message } = await createProduct(product);
-        console.log('success:', success)
-        console.log('message:', message)
-        setProduct({name: "", price: "", image: ""});
-    };
+  const { createProduct } = useProduct();
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+  const [alertSeverity, setAlertSeverity] = useState("success"); // or "error"
 
-    return (
-        <Container maxWidth="sm">
-            <Stack spacing={3}
-            >
-                
-                <Typography
-                    variant="h3"
-                    size="2rem"
-                    textAlign="center"
-                    
-                    mb={8}                    
-                >
-                    Create a new product
-                </Typography>
+  const handleProduct = async () => {
+    const { success, message } = await createProduct(product);
+    setAlertMessage(message);
+    setAlertSeverity(success ? "success" : "error");
+    setOpenSnackbar(true);
+  };
 
-                <Box w="full" p={6} rounded="lg" shadow="md">
-                    <Stack spacing={4}
-                    >
-                        <TextField
-                            id="product-name"
-                            label="Product Name"
-                            variant="outlined"
-                            value={product.name}
-                            onChange={(event) => setProduct({ ...product, name: event.target.value })}
-                        />
-                        <TextField
-                            id="price"
-                            label="Product Price"
-                            type="number"
-                            variant="outlined"
-                            value={product.price}
-                            onChange={(event) => setProduct({ ...product, price: event.target.value })}
-                        />
+  const handleCloseSnackbar = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpenSnackbar(false);
+  };
 
-                        <TextField
-                            id="product-image"
-                            label="Product image"
-                            variant="outlined"
-                            value={product.image}
-                            onChange={(event) => setProduct({ ...product, image: event.target.value })}
-                        />
-                        <Button color="success" variant="contained" width="full" onClick={handleProduct} w="full">
-                            Add Product
-                        </Button>
-                    </Stack>
-                </Box>
-            </Stack>
-        </Container>
-    );
+  return (
+    <Container maxWidth="sm">
+      <Stack spacing={3}>
+        <Typography variant="h3" textAlign="center" mb={8}>
+          Create a new product
+        </Typography>
+
+        <Box p={6} borderRadius="lg" boxShadow="md">
+          <Stack spacing={4}>
+            <TextField
+              id="product-name"
+              label="Product Name"
+              variant="outlined"
+              value={product.name}
+              onChange={(event) => setProduct({ ...product, name: event.target.value })}
+            />
+            <TextField
+              id="price"
+              label="Product Price"
+              type="number"
+              variant="outlined"
+              value={product.price}
+              onChange={(event) => setProduct({ ...product, price: event.target.value })}
+            />
+
+            <TextField
+              id="product-image"
+              label="Product image"
+              variant="outlined"
+              value={product.image}
+              onChange={(event) => setProduct({ ...product, image: event.target.value })}
+            />
+            <Button color="error" variant="contained" fullWidth onClick={handleProduct}>
+              Add Product
+            </Button>
+          </Stack>
+        </Box>
+      </Stack>
+      <Snackbar open={openSnackbar} autoHideDuration={6000} onClose={handleCloseSnackbar}>
+        <Alert onClose={handleCloseSnackbar} severity={alertSeverity} variant = "filled" sx={{ width: "100%" }}>
+          {alertMessage}
+        </Alert>
+      </Snackbar>
+    </Container>
+  );
 };
 
-export default createPage;
+export default CreatePage;
