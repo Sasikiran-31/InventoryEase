59309ddac68a1e5244e13920ec9e74865facee86
Sasikiran-31 <kirankumar.2001@yahoo.in>
2025-04-11 23:16:41 -0400

    More changes

--- frontend/front-end/src/pages/homePage.jsx
+++ frontend/front-end/src/pages/homePage.jsx
@@ -1,20 +1,47 @@
 import { useEffect } from "react";
-import { useProduct } from "../store/product";
+import { useStore } from "../store/product.js";
+import { Container, Grid, Stack, Typography } from "@mui/material";
 
 
 const homePage = () => {
-    const { fetchproducts, products } = useProduct();
+    const { fetchproducts, products } = useStore();
 
     useEffect( () => {
         fetchproducts();
     }, [fetchproducts]);
     console.log("products", products)
 
-    
+
 
     return (
-        <div>Homepage</div>
+        <Container maxWidth='xl'>
+            <Stack spacing={8}>
+                    <Typography
+                        fontSize={"30"}
+                        fontWeight={"bold"}
+                        bgcolor={"Menu"}
+                        textAlign={"center"}
+                    >
+                        ~Current Products in the Inventory~
+                    </Typography>
+                    <Grid
+                        columns={{
+                            base: 1,
+                            md: 2,
+                            lg: 3,
+                        }}
 
+                        spacing={10}
+                        width={"full"}
+                    >
+                        {products.map((product) => (
+                            <ProductCard key {...product._id} product = {product}/>
+                        ))}
+                    </Grid>
+            </Stack>
+
+        </Container>
+        
     );
 }
 
